115a101ad2eeafbabc53266229fb6cf0654ce78e
antoniogilsonbjr <gilsonantonio.bjr@gmail.com>
2025-11-24 19:41:44 -0300

    Update vite.config.ts
    Corrige erro de build do PeerJS no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true // Permite acesso via IP na rede local
-  }
+  plugins: [react( )],
+  // Adiciona esta seção para otimizar o PeerJS
+  optimizeDeps: {
+    include: ['peerjs'],
+  },
 })
